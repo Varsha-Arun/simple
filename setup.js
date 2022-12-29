9c4e1faf9762424edb6b0990ca1714fca7dfd780
Varsha-Arun <92varshaan@gmail.com>
2022-12-29 15:46:00 +0530

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,17 @@
 'use strict';
 
 module.exports = setup;
+async function setup(callback) {
+    await _installAndConfigureJenkins(VMIpAddress);
+  } catch (err) {
+    logger.error(who, 'Setup failed with error', util.inspect(err));
+    return callback(err);
+  }
+
+  logger.debug(who, 'complete');
+  callback();
+}
+
 const executeRemote = require('./executeRemoteUtils.js');
 let pemFileName = 'automation.pem';
 let scriptsDir = path.join(__dirname);
@@ -8,26 +19,7 @@ let pemFilePath = null;
 let VMIpAddress = process.env.STATIC_VM;
 let sshPrivateKey = process.env.SSH_PRIVATE_KEY;
 
-async function _createPemFileV2(who, sshPrivateKey) {
-  var who = 'setUp|_createPemFile';
-  logger.debug(who, 'Inside');
-  pemFilePath = path.join(scriptsDir, pemFileName);
-  console.log('Varsha Pem file Path : ', pemFilePath);
-
-  try {
-    await fsPromises.writeFile(pemFilePath, sshPrivateKey)
-    console.log("Varsha File written successfully");
-    console.log("Varsha The written file has"
-        + " the following contents:");
-  } catch(err) {
-    console.error(err);
-    return;
-  }
-}
-
-async function _installAndConfigureJenkins(who, VMIpAddress) {
-  who = who + '|_installAndConfigureJenkins';
-  logger.info(who, 'Inside');
+async function _installAndConfigureJenkins(VMIpAddress) {
 
   await _createPemFileV2(who, sshPrivateKey);
   console.log('Varsha Race condition');
@@ -81,97 +73,3 @@ async function _installAndConfigureJenkins(who, VMIpAddress) {
     console.error(err);
     return err;
   }
-
-  /*
-  await executeRemote.testLocal('cat', [pemFilePath], function cb(data, err) {
-    if(err) {
-      console.log('executeOnRemoteServer: ', err);
-      return;
-    } else {
-      if(data) {
-        _.each(data.toString().split('\n'),
-          function (consoleLine) {
-            if (!_.isEmpty(consoleLine)) {
-              console.log('Varsha : consoleLine',consoleLine);
-            }
-          });
-          return;
-        } else {
-          return;
-        }
-      }
-  });
-
-
-  await executeRemote.copy(VMIpAddress, '22', sshUser, pemFilePath, script, '/home/' + sshUser, async function cb(data, err) {
-    if(err) {
-      console.log('copyFilesToRemoteServer: ', err);
-      return;
-    } else {
-      console.log('before----varsha----executeRemote.run');
-      await executeRemote.run(VMIpAddress, '22', sshUser, pemFilePath, 'cat', script, function cb(data, err) {
-          if(err) {
-            console.log('executeOnRemoteServer: ', err);
-            return;
-          } else {
-            if(data) {
-              _.each(data.toString().split('\n'),
-                function (consoleLine) {
-                  if (!_.isEmpty(consoleLine)) {
-                    console.log('consoleLine-----run--vars',consoleLine);
-                    if(consoleLine.includes('JENKINS_API_TOKEN=')) {
-                      console.log('consoleLine ------------testvarsha',consoleLine);
-                      var key = string.split('=')[1];
-                      console.log('key----vars',key);
-                    }
-                  }
-                }
-              );
-              return;
-            } else {
-              return;
-            }
-        }
-    });
-    if(data) {
-        console.log(data);
-        return;
-    }
-    }
-  });
-*/
-  //sleep(30);
-
-  
-/*  executeRemote.copy(VMIpAddress, '22', sshUser, pemFilePath, script, '/home/' + sshUser, function cb(data, err) {
-    if(err) {
-      console.log('copyFilesToRemoteServer: ', err);
-    } else {
-      console.log('before----varsha----executeRemote.run');
-      executeRemote.run(VMIpAddress, '22', sshUser, pemFilePath, 'cat', script, function cb(data, err) {
-          if(err) {
-            console.log('executeOnRemoteServer: ', err);
-          } else {
-            if(data) {
-              _.each(data.toString().split('\n'),
-                function (consoleLine) {
-                  if (!_.isEmpty(consoleLine)) {
-                    console.log('consoleLine-----run--vars',consoleLine);
-                    if(consoleLine.includes('JENKINS_API_TOKEN=')) {
-                      console.log('consoleLine ------------testvarsha',consoleLine);
-                      var key = string.split('=')[1];
-                      console.log('key----vars',key);
-                    }
-                  }
-                }
-              );
-            }
-        }
-    });
-    if(data) {
-        console.log(data);
-    }
-    }
-  });
-*/
-}
